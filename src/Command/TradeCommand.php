fd259b9295de627a1b584890aac6b757ee67afb0
Дмитриев Лев <Lev.Dmitrievv@yandex.ru>
2024-09-25 20:05:27 +0400

    feat: Добавил старт консольной команды торговли и оптимизировал разворачивание

--- src/Command/TradeCommand.php
+++ src/Command/TradeCommand.php
@@ -6,12 +6,10 @@ namespace App\Command;
 
 use App\Entity\Coin;
 use App\Repository\CoinRepository;
-use App\Repository\PositionRepository;
 use App\TradingStrategy\TradingStrategyFactoryInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
