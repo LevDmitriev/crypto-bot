cdf621f7b73796f7118bdecef4619761bfe93eee
Дмитриев Лев <Lev.Dmitrievv@yandex.ru>
2024-10-10 16:03:09 +0400

    feat: Поправил нормализацию приказа т.к. API принимает лишь 4 символа после запятой.

--- src/Serializer/Normalizer/OrderNormalizer.php
+++ src/Serializer/Normalizer/OrderNormalizer.php
@@ -29,7 +29,7 @@ class OrderNormalizer implements NormalizerInterface
             'symbol' => $object->getSymbol(),
             'side' => $object->getSide()->value,
             'orderType' => $object->getType()->value,
-            'qty' => (string) $object->getQuantity(),
+            'qty' => bcadd('0', (string) $object->getQuantity(), 4), //API принимает лишь 4 числа после запятой
             'price' => $object->getType() === Order\ByBit\Type::Limit ? (string) $object->getPrice() : null,
             'triggerPrice' => $object->getTriggerPrice(),
         ];
